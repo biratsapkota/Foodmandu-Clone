265da1b43550f50a4149d193b98aba84e47ecaf4
biratsapkota <biratsapkota9@gmail.com>
2018-12-26 13:06:34 +0545

    some changes

--- src/components/specialdeal/specialdeal.jsx
+++ src/components/specialdeal/specialdeal.jsx
@@ -1,22 +1,53 @@
 import React, { Component } from 'react';
-import Specialbox from './specialbox'
+import SpecialBox from './specialbox'
 
 class specialdeal extends Component{
 	render(){
+		let specialdeal =[
+			{
+				name:"Flat Iron Grill",
+				image_url:"https://res.cloudinary.com/dpy0mkwsp/image/upload/v1545649415/images/Super7_Web_Feature_Listing__1_-min_251118033425.png"
+			},
+			{
+				name:"Flat Iron Grill",
+				image_url:"https://res.cloudinary.com/dpy0mkwsp/image/upload/v1545649415/images/Super7_Web_Feature_Listing__1_-min_251118033425.png"
+			},
+			{
+				name:"Flat Iron Grill",
+				image_url:"https://res.cloudinary.com/dpy0mkwsp/image/upload/v1545649415/images/Super7_Web_Feature_Listing__1_-min_251118033425.png"
+			},
+			{
+				name:"Flat Iron Grill",
+				image_url:"https://res.cloudinary.com/dpy0mkwsp/image/upload/v1545649415/images/Super7_Web_Feature_Listing__1_-min_251118033425.png"
+			},
+			{
+				name:"Flat Iron Grill",
+				image_url:"https://res.cloudinary.com/dpy0mkwsp/image/upload/v1545649415/images/Super7_Web_Feature_Listing__1_-min_251118033425.png"
+			},
+			{
+				name:"Flat Iron Grill",
+				image_url:"https://res.cloudinary.com/dpy0mkwsp/image/upload/v1545649415/images/Super7_Web_Feature_Listing__1_-min_251118033425.png"
+			},
+			{
+				name:"Flat Iron Grill",
+				image_url:"https://res.cloudinary.com/dpy0mkwsp/image/upload/v1545649415/images/Super7_Web_Feature_Listing__1_-min_251118033425.png"
+			},
+			{
+				name:"Flat Iron Grill",
+				image_url:"https://res.cloudinary.com/dpy0mkwsp/image/upload/v1545649415/images/Super7_Web_Feature_Listing__1_-min_251118033425.png"
+			}
+		]
+
+		let display_specialdeal = specialdeal.map(specialdeal =>
+				< SpecialBox name={specialdeal.name} image_url={specialdeal.image_url} />
+			) 
 		return(
 			<div className="specialdeal">
 				<div className = "specialhead">
 					SPECIAL DEALS
 				</div>
 				<div className="specialboxes">
-					<Specialbox name="Flat Iron Grill" link ="https://res.cloudinary.com/dpy0mkwsp/image/upload/v1545649415/images/Super7_Web_Feature_Listing__1_-min_251118033425.png"/>
-					<Specialbox name="Flat Iron Grill" link ="https://res.cloudinary.com/dpy0mkwsp/image/upload/v1545649415/images/Super7_Web_Feature_Listing__1_-min_251118033425.png"/>
-					<Specialbox name="Flat Iron Grill" link ="https://res.cloudinary.com/dpy0mkwsp/image/upload/v1545649415/images/Super7_Web_Feature_Listing__1_-min_251118033425.png"/>
-					<Specialbox name="Flat Iron Grill" link ="https://res.cloudinary.com/dpy0mkwsp/image/upload/v1545649415/images/Super7_Web_Feature_Listing__1_-min_251118033425.png"/>
-					<Specialbox name="Flat Iron Grill" link ="https://res.cloudinary.com/dpy0mkwsp/image/upload/v1545649415/images/Super7_Web_Feature_Listing__1_-min_251118033425.png"/>
-					<Specialbox name="Flat Iron Grill" link ="https://res.cloudinary.com/dpy0mkwsp/image/upload/v1545649415/images/Super7_Web_Feature_Listing__1_-min_251118033425.png"/>
-					<Specialbox name="Flat Iron Grill" link ="https://res.cloudinary.com/dpy0mkwsp/image/upload/v1545649415/images/Super7_Web_Feature_Listing__1_-min_251118033425.png"/>
-					<Specialbox name="Flat Iron Grill" link ="https://res.cloudinary.com/dpy0mkwsp/image/upload/v1545649415/images/Super7_Web_Feature_Listing__1_-min_251118033425.png"/>
+					{ display_specialdeal }
 				</div>
 				<div className = "viewdeal">
 					View all Deals -->
